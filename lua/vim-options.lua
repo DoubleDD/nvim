7c247dbab0aec2cdf956d8a6cdddc7def511e6f8
kedong <kedong@yunlizhi.com>
2024-06-14 18:09:29 +0800

    settings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,8 +2,10 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set clipboard=unnamed")
 vim.g.mapleader = " "
-vim.g.background = "light"
+-- vim.g.background = "light"
+vim.g.transparent_background = true
 
 vim.opt.swapfile = false
 
