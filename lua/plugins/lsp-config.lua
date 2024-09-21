66cf6dfaa5813072bdb7c57a140087037328e3f5
kedong <kedong@yunlizhi.com>
2024-09-22 04:13:59 +0800

    .

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -59,4 +59,55 @@ return {
       })
     end,
   },
+  {
+    "onsails/lspkind-nvim",
+    config = function()
+      local lspkind = require("lspkind")
+      lspkind.init({
+        mode = "symbol", -- 显示图标和文本
+        maxwidth = 50, -- 最大宽度
+        ellipsis_char = "...", -- 超出部分显示省略号
+        preset = "default", -- 预设样式
+        symbol_map = {
+          Text = "󰉿",
+          Method = "󰆧",
+          Function = "󰊕",
+          Constructor = "",
+          Field = "󰜢",
+          Variable = "󰀫",
+          Class = "󰠱",
+          Interface = "",
+          Module = "",
+          Property = "󰜢",
+          Unit = "󰑭",
+          Value = "󰎠",
+          Enum = "",
+          Keyword = "󰌋",
+          Snippet = "",
+          Color = "󰏘",
+          File = "󰈙",
+          Reference = "󰈇",
+          Folder = "󰉋",
+          EnumMember = "",
+          Constant = "󰏿",
+          Struct = "󰙅",
+          Event = "",
+          Operator = "󰆕",
+          TypeParameter = "",
+        },
+      })
+
+      require("cmp").setup({
+        formatting = {
+          fields = { "abbr", "kind", "menu" },
+          format = lspkind.cmp_format({
+            mode = "symbol",
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+      })
+    end,
+    dependencies = { "hrsh7th/nvim-cmp" }, -- 确保与 nvim-cmp 兼容
+  },
 }
