01c7460ef8b6e709ff3a4b654d762c906f19643b
kedong <kedong@yunlizhi.com>
2024-12-23 11:00:48 +0800

    使用lsp给caddyfile做增强，删除formatting、linting

--- lua/plugins/caddy.lua
+++ lua/plugins/caddy.lua
@@ -1,4 +1,22 @@
 return {
   "isobit/vim-caddyfile",
-  config = function() end,
+  config = function()
+    local client = vim.lsp.start_client({
+      name = "caddyfilelsp",
+      cmd = { "/Volumes/code/com.github/DoubleDD/learnlsp/main" },
+      on_attach = function(client, bufnr) end,
+    })
+
+    if not client then
+      vim.notify("hey ,you didnt do the client thing good")
+      return
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "caddyfile123",
+      callback = function()
+        vim.lsp.buf_attach_client(0, client)
+      end,
+    })
+  end,
 }
--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -26,7 +26,7 @@ return {
       if not configs.caddy_lsp then
         configs.caddy_lsp = {
           default_config = {
-            cmd = { "/Users/kedong/code/com.github/caddy/caddyfile-language-server/caddy-lsp" },
+            cmd = { "/Volumes/code/com.github/DoubleDD/learnlsp/main" },
             filetypes = { "caddyfile" },
             root_dir = function(fname)
               return lspconfig.util.find_git_ancestor(fname) or vim.loop.os_homedir()
@@ -36,30 +36,9 @@ return {
         }
       end
       -- 启动 LSP
-      -- lspconfig.caddy_lsp.setup({
-      --   capabilities = require("cmp_nvim_lsp").default_capabilities(),
-      --   on_attach = function(client, bufnr)
-      --     -- 启用自动补全
-      --     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-      -- -- 键位映射
-      -- local opts = { noremap=true, silent=true, buffer=bufnr }
-      -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-      -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-      -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-      -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-      -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-      -- vim.keymap.set('n', '<space>wl', function()
-      --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      -- end, opts)
-      -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-      -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-      -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-      -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-      --   end,
-      -- })
+      lspconfig.caddy_lsp.setup({
+        capabilities = capabilities,
+      })
       lspconfig.solargraph.setup({
         capabilities = capabilities,
       })
